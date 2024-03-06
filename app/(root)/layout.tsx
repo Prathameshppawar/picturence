ae4b715a519ed9ad26c487e0504466b6a71b139e
Prathamesh Pawar <95527969+Prathameshppawar@users.noreply.github.com>
2024-03-07 00:20:07 +0530

    Added the sidebar
    Sidebar added, not checked the routes for their functionality
    UI now looks fine

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,11 @@
+import Sidebar from "@/components/shared/sidebar"
 import React from "react"
 
 const layout = ({children}: {children: React.ReactNode}) => {
   return (
     <main className="root">
+      {/*we have to implement here sidebar and the mobile nav*/}
+      <Sidebar />
         <div className="root-container">
             <div className="wrapper">
                 {children}
