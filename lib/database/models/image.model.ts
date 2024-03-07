27c7bf946f96caa230629fdbee94cc452f65dfe3
Prathamesh Pawar <95527969+Prathameshppawar@users.noreply.github.com>
2024-03-07 23:53:09 +0530

    All the three models added

---
+++ lib/database/models/image.model.ts
@@ -0,0 +1,45 @@
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IImage extends Document{
+    title: string;
+    transformationtype: string;
+    publicId: string;
+    secureUrl: string;
+    width?: number; // Optional property
+    height?: number; // Optional property
+    config?: object; // Optional property
+    transformationUrl?: URL; // Optional property
+    asepectRatio?: string; // Optional property
+    color?: string; // Optional property
+    prompt?: string; // Optional property
+    author: {
+        _id: String,
+        firstName: String,
+        lastName: String
+    }
+    createdAt: Date;
+    updatedAt: Date;
+  }
+  
+
+const ImageSchema = new Schema({
+    title: {type: String, required: true},
+    transformationtype: { type: String, required: true},
+    publicId: {type: String, required: true},
+    secureUrl: { type: String, required: true},
+    width: { type: Number},
+    height: { type :Number}, 
+    config: { type: Object},
+    transformationUrl: { type: URL},
+    asepctRatio: { type: String}, 
+    color: {type: String},
+    prompt: {type: String},
+    author: { type: Schema.Types.ObjectId, ref: 'User'},
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date, default: Date.now}
+})
+
+const Image = models?.Image || model('Image', ImageSchema);
+
+
+export default Image;
