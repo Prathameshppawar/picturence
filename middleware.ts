05779a827babd67490bd8377846585851802abf3
Prathamesh Pawar <95527969+Prathameshppawar@users.noreply.github.com>
2024-03-09 04:47:55 +0530

    Revert "Li'l changes"
    This reverts commit ac9117c4531a4fc2c5b16842a6c00a69a280173a.

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+  publicRoutes: ['/', '/api/webhooks/(.*)', '/api/webhooks/clerk', '/api/webhooks/stripe']
 });
- 
+ console.log('called midddleware')
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
