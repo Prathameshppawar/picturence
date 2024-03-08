ffad956d693a5c9102f6c163f2e4781bec9c698d
Prathamesh Pawar <95527969+Prathameshppawar@users.noreply.github.com>
2024-03-09 04:51:29 +0530

    Revert "Revert "Li'l changes""
    This reverts commit 05779a827babd67490bd8377846585851802abf3.

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,33 +1,32 @@
-import mongoose, {Mongoose} from "mongoose";
-import { cache } from "react";
+import mongoose, { Mongoose } from 'mongoose';
 
-const MONGODB_URL= process.env.MONGODBURL;
-interface MongooseConnection{
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
-}
-console.log('CALLED FOR CONNECTIONS')
-let cached: MongooseConnection= (global as any).mongoose
+const MONGODB_URL = process.env.MONGODB_URL;
 
-if(!cached){
-    cached: (global as any).mongoose= {
-        conn: null, promise: null
-    }
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
+let cached: MongooseConnection = (global as any).mongoose
 
-export const connectToDatabase= async()=>{
-    if(cached.conn) return cached.conn;
-    if(!MONGODB_URL) throw new Error('Missing mongourl');
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
+}
 
-    cached.promise= 
-        cached.promise || 
-        mongoose.connect(
-            MONGODB_URL, {
-            dbName: 'picturence', bufferCommands: false
-        }) 
+export const connectToDatabase = async () => {
+  if(cached.conn) return cached.conn;
 
-    cached.conn= await cached.promise
-    console.log(cached)
-    return cached.conn;
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'imaginify', bufferCommands: false 
+    })
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
